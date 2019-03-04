dc78b00aa3604b926a5842e7b3112638e2421284
Eduard Klygunov <nostrumee@gmail.com>
2019-03-04 16:44:44 +0300

    advanced-zeros task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let multipliers = {};
+  let multiplier = 2;
+  let baseValue = base;
+  let sqrt = Math.sqrt(base);
+  let result = Infinity;
+
+  while (baseValue > 1 && multiplier <= sqrt) {
+    if (baseValue % multiplier === 0) {
+      if (multipliers[multiplier] === undefined) {
+        multipliers[multiplier] = 1;
+      } else {
+        multipliers[multiplier]++;
+      }
+      baseValue /= multiplier;
+    } else if (multiplier === 2) {
+      multiplier++;
+    } else {
+      multiplier += 2;
+    }
+  }
+
+  if (baseValue != 1) {
+    multipliers[baseValue] = 1;
+  }
+
+  for (key in multipliers) {
+    let divisor = key;
+    let currentResult = 0;
+
+    while (number / divisor > 1) {
+      currentResult += Math.floor(number / divisor);
+      divisor *= key;
+    }
+
+    currentResult = Math.floor(currentResult / multipliers[key]);
+
+    result = Math.min(currentResult, result);
+  }
+
+  return result;
 }
